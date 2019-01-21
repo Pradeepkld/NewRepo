5f48f289dc34d439eccc70ac10e01c6bfbeaa4ff
DEEPU <DEEPU@DEEPUHOME>
2019-01-21 13:05:44 +0530

    initial push

---
+++ HelloWorldForGIT/HelloWorldForGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldForGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+        }
+    }
+}
